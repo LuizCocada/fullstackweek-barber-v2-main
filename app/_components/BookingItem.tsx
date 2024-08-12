1636331a1c218f8be9dcbfee202f6e2ec5b68ca1
luizribeiro <ludehenriq@gmail.com>
2024-08-12 10:42:30 -0300

    add bookingItem component

---
+++ app/_components/BookingItem.tsx
@@ -0,0 +1,40 @@
+import { Card, CardContent } from "./ui/card"
+import { Badge } from "./ui/badge"
+import { Avatar, AvatarImage } from "./ui/avatar"
+
+//TUDO: receber agendamento com prop
+const BookingItem = () => {
+    return(
+        <>
+          <h2 className="text-xs font-bold text-gray-400 mt-5">
+            AGENDAMENTOS
+          </h2>
+
+          <div>
+            <Card className="mt-6">
+              <CardContent className="flex justify-between p-0">
+                <div className="flex flex-col gap-2 py-5 pl-5">
+                  <Badge className="w-fit">Confirmado</Badge>
+                  <h3 className="font-semibold">Corte de Cabelo</h3>
+
+                  <div className="flex items-center gap-2">
+                    <Avatar className="h-6 w-6">
+                      <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
+                    </Avatar>
+                    <p className="text-sm">Vintage Barber</p>
+                  </div>
+                </div>
+
+                <div className="flex flex-col justify-center items-center px-5 border-l">
+                  <p className="text-sm">Agosto</p>
+                  <p className="text-2xl">05</p>
+                  <p className="text-sm">20:00</p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </>
+    )
+}
+
+export default BookingItem
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
 import { Input } from "./_components/ui/input"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarberShopItem from "./_components/BarberShopItem"
 import {quickSearchOptions} from "./_constants/search"
+import BookingItem from "./_components/BookingItem"
 
 const Home = async () => {
 
@@ -59,34 +58,7 @@ const Home = async () => {
           </div>
 
           {/* AGENDAMENTO => Booking */}
-          {/* FAZER COMPONENTE DE AGENDAMENTO */}
-          <h2 className="text-xs font-bold text-gray-400 mt-5">
-            AGENDAMENTOS
-          </h2>
-
-          <div>
-            <Card className="mt-6">
-              <CardContent className="flex justify-between p-0">
-                <div className="flex flex-col gap-2 py-5 pl-5">
-                  <Badge className="w-fit">Confirmado</Badge>
-                  <h3 className="font-semibold">Corte de Cabelo</h3>
-
-                  <div className="flex items-center gap-2">
-                    <Avatar className="h-6 w-6">
-                      <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
-                    </Avatar>
-                    <p className="text-sm">Vintage Barber</p>
-                  </div>
-                </div>
-
-                <div className="flex flex-col justify-center items-center px-5 border-l">
-                  <p className="text-sm">Agosto</p>
-                  <p className="text-2xl">05</p>
-                  <p className="text-sm">20:00</p>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
+          <BookingItem /> 
 
           <h2 className="text-xs font-bold text-gray-400 mt-6 pb-5">
             RECOMENDADOS
